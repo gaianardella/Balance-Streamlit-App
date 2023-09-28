3e9bb8c855fa8f2bbad4befd4ebc84ada644766f
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-09-29 00:13:50 +0200

    Update streamlit-balance.py

--- streamlit-balance.py
+++ streamlit-balance.py
@@ -109,7 +109,7 @@ def main():
     single_balances = []
     
     # Iterate through the keys (dates) in the session_state dictionary
-    for date, expenses in at.session_state.items():
+    for date, expenses in st.session_state.items():
         st.write(date)
         st.write(expenses)
     #     # Calculate the sum of expenses for the current date
