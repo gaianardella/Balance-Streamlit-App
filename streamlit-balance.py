eeac8c094cfbc38145baddee78947d4388ded95a
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-09-29 00:45:38 +0200

    Update streamlit-balance.py

--- streamlit-balance.py
+++ streamlit-balance.py
@@ -108,9 +108,9 @@ def main():
                 # Example: The starting and ending dates
                 last_key = list(st.session_state.keys())[-1].split('/')
                 st.write(last_key)
-                # start_date = datetime(last_key[2], last_key[0], last_key[1])
+                start_date = datetime(int(last_key[2]), int(last_key[0]), int(last_key[1]))
                 end_date = datetime(year, day, month)
-                # st.write(start_date)
+                st.write(start_date)
                 st.write(end_date)
     st.write(st.session_state)
     # Calculate the range of dates
