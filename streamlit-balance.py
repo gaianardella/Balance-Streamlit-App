0d7def8da59940bd530ef68780fac76f6253af7d
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-09-29 00:47:59 +0200

    Update streamlit-balance.py

--- streamlit-balance.py
+++ streamlit-balance.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # class BudgetManager:
 #     # set daily goal, expenses list and balance
