f75c42698bdf60d0f1380eb8dd7c26d632cce537
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-09-28 23:35:45 +0200

    Update streamlit-balance.py

--- streamlit-balance.py
+++ streamlit-balance.py
@@ -87,7 +87,7 @@ def main():
     
     if st.button('Aggiungi spesa'):
         # budget_manager.add_expense(expense, option)
-        st.session_state['expenses'][date].append((date, expense, category))
+        st.session_state['expenses'].append((date, expense, category))
         
     st.text("2. Visualizza stato")
     # current_balance = budget_manager.balance
